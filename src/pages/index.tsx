8983957371c02846c56adab2975abe1a6b56502d
LeonardoMessias98 <messiasleonardo98@gmail.com>
2022-01-10 21:26:06 -0300

    feat: :sparkles: some adjusts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/login">Login</Link>
+    </>
+  );
 };
 
 export default Home;
